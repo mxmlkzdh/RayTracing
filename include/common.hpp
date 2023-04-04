0b75099ab904fae8f6a681025495d9257ef4819f
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-04 19:34:13 -0400

    Add randomDouble()

--- include/common.hpp
+++ include/common.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <limits>
+#include <cstdlib>
 
 namespace RayTracing {
 
@@ -8,6 +9,16 @@ const double PI = 3.1415926535897932385;
 
 double degreesToRadians(const double degrees) {
     return degrees * PI / 180.0;
+}
+
+double randomDouble() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+double randomDouble(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * randomDouble();
 }
 
 }
