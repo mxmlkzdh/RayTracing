bae22e8d298300a12c45a3418dd0460a925006e5
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-04 19:30:37 -0400

    Fix common issues

--- include/common.hpp
+++ include/common.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include <limits>
 
-const double infinity = std::numeric_limits<double>::infinity();
+namespace RayTracing {
+
+const double INFINITY = std::numeric_limits<double>::infinity();
 const double PI = 3.1415926535897932385;
 
 double degreesToRadians(const double degrees) {
-    return degrees * pi / 180.0;
+    return degrees * PI / 180.0;
+}
+
 }
