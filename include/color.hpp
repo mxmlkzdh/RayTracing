e971bcdae1986d1c0fbebcef769b12a502064fbd
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-04 21:39:36 -0400

    Add Gamma correction

--- include/color.hpp
+++ include/color.hpp
@@ -12,9 +12,9 @@ void writeColor(std::ofstream& outputFile, const Color& pixelColor, const int sa
     double b = pixelColor.z;
     // Divide the color by the number of samples.
     const double scale = 1.0 / samplesPerPixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = std::sqrt(r * scale);
+    g = std::sqrt(g * scale);
+    b = std::sqrt(b * scale);
     outputFile << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
