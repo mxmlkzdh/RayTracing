21915011251fa8b85536acaa23b5ac38d1ad8649
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-05 19:29:40 -0400

    Start fresh

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <spdlog/spdlog.h>
+#include <iostream>
+#include <fstream>
+
+#define OUTPUT_FILE "/Users/max/development/cpp/raytracing/data/output.ppm"
+
+int main(int, char const**) {
+
+}
