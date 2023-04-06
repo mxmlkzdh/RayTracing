fe1a12f7aa526de1cbc3fc012d6218bd03de1d29
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-06 18:30:28 -0400

    Add RayTracing::Object

---
+++ include/object.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "ray.hpp"
+#include "vector3.hpp"
+
+namespace RayTracing {
+
+struct HitRecord {
+    double time;
+    Point point;
+    Vector3 normal;
+};
+
+class Object {
+
+    virtual bool hit(const Ray& ray, const double min, const double max, HitRecord& record) const = 0;
+    virtual ~Object() = 0;
+
+};
+
+}
