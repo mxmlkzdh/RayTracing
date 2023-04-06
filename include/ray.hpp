664bc2905bfc0094ff88647c21ec2b7f7c0486ca
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-05 21:33:51 -0400

    Add RayTracing::Ray

---
+++ include/ray.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "vector3.hpp"
+
+namespace RayTracing {
+
+struct Ray {
+
+    Point origin;
+    Vector3 direction;
+
+    Ray(const Point& origin, const Vector3& direction):origin(origin), direction(direction) {}
+    Point at(const double t) {
+        return origin + t * direction;
+    }
+
+};
+
+}
