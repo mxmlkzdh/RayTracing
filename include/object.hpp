104244859d2c7ea3399e9568ed2e9854855c9e8b
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-06 18:46:47 -0400

    Formatting

--- include/object.hpp
+++ include/object.hpp
@@ -12,11 +12,9 @@ struct HitRecord {
 
 // The interface for a hittable object
 class Object {
-
 public:
     virtual bool hit(const Ray& ray, const double min, const double max, HitRecord& record) const = 0;
     virtual ~Object() = 0;
-
 };
 
 }
