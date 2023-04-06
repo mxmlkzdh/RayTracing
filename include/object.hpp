3838830f748434c6e04becd3f9e724e86f46e419
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-06 19:15:42 -0400

    Made the Object's destructor virtual

--- include/object.hpp
+++ include/object.hpp
@@ -14,7 +14,7 @@ struct HitRecord {
 class Object {
 public:
     virtual bool hit(const Ray& ray, const double min, const double max, HitRecord& record) const = 0;
-    virtual ~Object() = 0;
+    virtual ~Object() {}
 };
 
 }
