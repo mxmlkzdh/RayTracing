baef2c639638504bf2f453445abc2214b36c5f42
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-06 18:17:41 -0400

    Mark the at method as const

--- include/ray.hpp
+++ include/ray.hpp
@@ -9,7 +9,7 @@ struct Ray {
     Vector3 direction;
 
     Ray(const Point& origin, const Vector3& direction):origin(origin), direction(direction) {}
-    Point at(const double t) {
+    Point at(const double t) const {
         return origin + t * direction;
     }
 
