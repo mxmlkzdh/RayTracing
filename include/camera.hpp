53a61032028bad3cd72415bcc8a1ddc2de0270cf
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-08 19:00:20 -0400

    Add support for positioning and orienting the Camera

--- include/camera.hpp
+++ include/camera.hpp
@@ -14,15 +14,20 @@ private:
     Vector3 VERTICAL;
     Vector3 LOWER_LEFT_CORNER;
 public:
-    Camera(const double vFoV, const double aspectRatio) { // Vertical field-of-view in degrees
+    Camera(const Point& lookFrom, const Point& lookAt, const Vector3& vUp, const double vFoV, const double aspectRatio) { // Vertical field-of-view in degrees
         const double THETA = Util::degreesToRadians(vFoV);
         const double H = std::tan(THETA / 2);
         const double VIEWPORT_HEIGHT = 2 * H;
         const double VIEWPORT_WIDTH = VIEWPORT_HEIGHT * aspectRatio;
-        ORIGIN = Point(0, 0, 0);
-        HORIZONTAL = Vector3(VIEWPORT_WIDTH, 0, 0);
-        VERTICAL = Vector3(0, VIEWPORT_HEIGHT, 0);
-        LOWER_LEFT_CORNER = ORIGIN - (HORIZONTAL / 2) - (VERTICAL / 2) - Vector3(0, 0, FOCAL_LENGTH);
+
+        Vector3 w = unitDirection(lookFrom - lookAt);
+        Vector3 u = unitDirection(cross(vUp, w));
+        Vector3 v = cross(w, u);
+
+        ORIGIN = lookFrom;
+        HORIZONTAL = VIEWPORT_WIDTH * u;
+        VERTICAL = VIEWPORT_HEIGHT * v;
+        LOWER_LEFT_CORNER = ORIGIN - (HORIZONTAL / 2) - (VERTICAL / 2) - w;
     }
     Ray getRay(const double u, const double v) const {
         return Ray(ORIGIN, LOWER_LEFT_CORNER + (u * HORIZONTAL) + (v * VERTICAL) - ORIGIN);
