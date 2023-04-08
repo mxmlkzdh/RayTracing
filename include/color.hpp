f0fb5479efe1a1ce881dd7a39a9fc24c5e8ee4bb
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-08 11:57:26 -0400

    Add gamma correction with gamma=2.0

--- include/color.hpp
+++ include/color.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <fstream>
+#include <cmath>
 #include "util.hpp"
 #include "object.hpp"
 #include "vector3.hpp"
@@ -8,9 +9,10 @@ namespace RayTracing {
 
 // Write the translated [0,255] value of each color component.
 void writePixel(std::ofstream& outputFile, const Color& color, const int samplesPerPixel) {
-    const double r = color.x / samplesPerPixel;
-    const double g = color.y / samplesPerPixel;
-    const double b = color.z / samplesPerPixel;
+    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+    const double r = std::sqrt(color.x / samplesPerPixel);
+    const double g = std::sqrt(color.y / samplesPerPixel);
+    const double b = std::sqrt(color.z / samplesPerPixel);
     outputFile
         << static_cast<int>(256 * Util::clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * Util::clamp(g, 0.0, 0.999)) << ' '
