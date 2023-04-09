4c54cbaff06ae0e430588f161a66632a64411460
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-09 11:15:44 -0400

    Add SolidColor

---
+++ include/texture/solid_color.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "../vector3.hpp"
+#include "../texture.hpp"
+
+namespace RayTracing {
+
+class SolidColor: public Texture {
+public:
+    Color color;
+public:
+    SolidColor() {}
+    SolidColor(const Color& color): color(color) {}
+    SolidColor(const double red, const double green, const double blue) {
+        color = Color(red, green, blue);
+    }
+    virtual ~SolidColor() {}
+    virtual Color value(const double u, const double v, const Point& point) const override {
+        return color;
+    }
+};
+
+}
