a16fed394e5f4b58832bacaef5befb9dfe19a718
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-09 19:03:08 -0400

    Update the Ray class for more readability

--- include/ray.hpp
+++ include/ray.hpp
@@ -11,7 +11,7 @@ public:
     Ray() {}
     Ray(const Point& origin, const Vector3& direction):origin(origin), direction(direction) {}
     Point at(const double t) const {
-        return origin + t * direction;
+        return origin + (t * direction);
     }
 };
 
