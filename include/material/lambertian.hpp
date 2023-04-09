c6f2496f45fda257530a1d73c4844c92da7f1029
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-09 11:19:19 -0400

    Add suppoert for Texture to Lambertian

--- include/material/lambertian.hpp
+++ include/material/lambertian.hpp
@@ -3,14 +3,17 @@
 #include "../vector3.hpp"
 #include "../object.hpp"
 #include "../material.hpp"
+#include "../texture.hpp"
+#include "../texture/solid_color.hpp"
 
 namespace RayTracing {
 
 class Lambertian: public Material {
 public:
-    Color albedo;
+    std::shared_ptr<Texture> albedo;
 public:
-    Lambertian(const Color& albedo): albedo(albedo) {};
+    Lambertian(const Color& albedo): albedo(std::make_shared<SolidColor>(albedo)) {}
+    Lambertian(const std::shared_ptr<Texture> albedo): albedo(albedo) {}
     virtual ~Lambertian() {}
     virtual bool scatter(const Ray&, const HitRecord& record, Color& attenuation, Ray& scatteredRay) const override {
         Vector3 scatterDirection = record.normal + randomInUnitSphere(); // Replace with randomUnitVector() for true Lambertian
@@ -18,7 +21,7 @@ public:
             scatterDirection = record.normal;
         }
         scatteredRay = Ray(record.point, scatterDirection);
-        attenuation = albedo;
+        attenuation = albedo->value(record.u, record.v, record.point);
         return true;
     }
 };
