eb84a72a6307ad34107c47a6d5086385dc70438c
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-10 17:10:09 -0400

    Update main to use RayTracing::Scene

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <argh/argh.h>
 #include <iostream>
 #include <fstream>
-#include <memory>
 #include <string>
 #include "util.hpp"
 #include "ray.hpp"
@@ -10,13 +9,8 @@
 #include "camera.hpp"
 #include "constants.hpp"
 #include "engine.hpp"
-#include "geometry/sphere.hpp"
+#include "scene.hpp"
 #include "geometry/world.hpp"
-#include "material/lambertian.hpp"
-#include "material/metal.hpp"
-#include "material/dielectric.hpp"
-#include "texture/image_texture.hpp"
-#include "texture/checker_texture.hpp"
 
 #define DEFAULT_OUTPUT_FILE_PATH "data/output.ppm"
 
@@ -46,22 +40,7 @@ int main(int argc, char const* argv[]) {
     const int MAX_DEPTH = 50;
 
     // World
-    RayTracing::World world;
-    auto earthTexture = std::make_shared<RayTracing::ImageTexture>("data/earthmap.jpg");
-    auto earthSurface = std::make_shared<RayTracing::Lambertian>(earthTexture);
-    auto checkerGround = std::make_shared<RayTracing::CheckerTexture>(RayTracing::Color(0.1, 0.1, 0.1), RayTracing::Color(0.99, 0.99, 0.99));
-    auto materialGround = std::make_shared<RayTracing::Lambertian>(checkerGround);
-    auto materialCenter = std::make_shared<RayTracing::Lambertian>(RayTracing::Color(0.0, 0.1, 0.3));
-    auto materialLeft = std::make_shared<RayTracing::Metal>(RayTracing::Color(0.75, 0.0, 0.0));
-    auto materialRight = std::make_shared<RayTracing::Metal>(RayTracing::Color(0.75, 0.5, 0.25));
-    auto materialFront = std::make_shared<RayTracing::Dielectric>(1.5);
-    auto materialFrontFront = std::make_shared<RayTracing::Lambertian>(RayTracing::Color(10.0 / 256, 35.0 / 256, 10.0 / 256));
-    world.add(std::make_shared<RayTracing::Sphere>(RayTracing::Point(0, -100.5, -1), 100, materialGround));
-    world.add(std::make_shared<RayTracing::Sphere>(RayTracing::Point(0, 0, -1.35), 0.5, earthSurface));
-    world.add(std::make_shared<RayTracing::Sphere>(RayTracing::Point(-1.0, 0.0, -1), 0.5, materialLeft));
-    world.add(std::make_shared<RayTracing::Sphere>(RayTracing::Point(0.825, -0.1, -0.65), 0.4, materialRight));
-    world.add(std::make_shared<RayTracing::Sphere>(RayTracing::Point(-0.25, -0.25, -0.25), 0.25, materialFront));
-    world.add(std::make_shared<RayTracing::Sphere>(RayTracing::Point(0.25, -0.4, -0.5), 0.1, materialFrontFront));
+    RayTracing::World world = RayTracing::Scene().getWorld();
 
     // Camera
     const RayTracing::Point lookFrom(0, 0, 1.5);
