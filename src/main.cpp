6a5cfeaebe47b02d3c0471e0d66348f3b6d21838
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-10 17:26:25 -0400

    Update main to use RayTracing::Image

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 // #include <spdlog/spdlog.h>
-#include <argh/argh.h>
+// #include <argh/argh.h>
 #include <iostream>
 #include <fstream>
-#include <string>
 #include "util.hpp"
 #include "ray.hpp"
 #include "color.hpp"
@@ -10,34 +9,19 @@
 #include "constants.hpp"
 #include "engine.hpp"
 #include "scene.hpp"
+#include "image.hpp"
 #include "geometry/world.hpp"
 
 #define DEFAULT_OUTPUT_FILE_PATH "data/output.ppm"
 
-int main(int argc, char const* argv[]) {
+int main(int, char const**) {
 
     // Automatically benchmarks the execution of this code block (main) in seconds.
     RayTracing::Util::Timer timer;
 
-    // Command line arguments parser
-    argh::parser cmdl = argh::parser(argc, argv);
-
     // Image
-    std::string outputFilePath;
-    cmdl({"-o", "--output"}, DEFAULT_OUTPUT_FILE_PATH) >> outputFilePath;
     const double ASPECT_RATIO = 16.0 / 9.0;
-    int width;
-    if (cmdl[{"-h"}]) { // For Full HD Resolution in 16/9 Aspect Ratio
-        width = 1920;
-    } else {
-        cmdl({"-w", "--width"}, 400) >> width;
-    }
-    const int IMAGE_WIDTH = width;
-    const int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
-    int samples;
-    cmdl({"-s", "--samples"}, 10) >> samples;
-    const int SAMPLES_PER_PIXEL = samples;
-    const int MAX_DEPTH = 50;
+    const RayTracing::Image image(DEFAULT_OUTPUT_FILE_PATH, 1200, ASPECT_RATIO);
 
     // World
     RayTracing::World world = RayTracing::Scene().getWorld();
@@ -50,18 +34,21 @@ int main(int argc, char const* argv[]) {
     RayTracing::Camera camera(lookFrom, lookAt, vUp, vFoV, ASPECT_RATIO);
 
     // Render
+    const int SAMPLES_PER_PIXEL = 10;
+    const int MAX_DEPTH = 50;
+
     std::ofstream outputFile;
-    outputFile.open(outputFilePath);
+    outputFile.open(image.fileName);
     if (outputFile.is_open()) {
-        outputFile << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << '\n' << "255\n";
-        std::cout << "Image Dimensions: " << IMAGE_WIDTH << " x " << IMAGE_HEIGHT << " | Samples Per Pixel: " << SAMPLES_PER_PIXEL << std::endl;
-        for (int j = IMAGE_HEIGHT - 1; j >= 0; j--) {
-            std::cout << "\rProgress: " << 100 - static_cast<int>((static_cast<double>(j) / (IMAGE_HEIGHT - 1)) * 100) << "% " << std::flush;
-            for (int i = 0; i < IMAGE_WIDTH; i++) {
+        outputFile << "P3\n" << image.width << ' ' << image.height << '\n' << "255\n";
+        std::cout << "Image Dimensions: " << image.width << " x " << image.height << " | Samples Per Pixel: " << SAMPLES_PER_PIXEL << std::endl;
+        for (int j = image.height - 1; j >= 0; j--) {
+            std::cout << "\rProgress: " << 100 - static_cast<int>((static_cast<double>(j) / (image.height - 1)) * 100) << "% " << std::flush;
+            for (int i = 0; i < image.width; i++) {
                 RayTracing::Color color(0, 0, 0);
                 for (int k = 0; k < SAMPLES_PER_PIXEL; k++) {
-                    double u = (static_cast<double>(i) + RayTracing::Util::random()) / (IMAGE_WIDTH - 1);
-                    double v = (static_cast<double>(j) + RayTracing::Util::random()) / (IMAGE_HEIGHT - 1);
+                    double u = (static_cast<double>(i) + RayTracing::Util::random()) / (image.width - 1);
+                    double v = (static_cast<double>(j) + RayTracing::Util::random()) / (image.height - 1);
                     RayTracing::Ray ray = camera.getRay(u, v);
                     color = color + RayTracing::computeRayColor(ray, world, MAX_DEPTH);
                 }
