26baa01d10b555ec9cda0656e658814a488fcd13
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-10 17:23:21 -0400

    Implement RayTracing::Image

--- include/image.hpp
+++ include/image.hpp
@@ -1,10 +1,20 @@
 #pragma once
+#include <string>
 
 namespace RayTracing {
 
 class Image {
 public:
+    std::string fileName;
+    int width;
+    int height;
 public:
+    Image(const std::string fileName, const int width, const int height):
+        fileName(fileName), width(width), height(height) {
+    }
+    Image(const std::string fileName, const int width, const double aspectRatio):
+        fileName(fileName), width(width), height(static_cast<int>(width / aspectRatio)) {
+    }
 };
 
 }
