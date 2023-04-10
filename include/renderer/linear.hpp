e02d21435018b8f28a9c855fbc22e18038490bba
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-10 18:17:19 -0400

    Move the coloring and writing logic to the renderer

--- include/renderer/linear.hpp
+++ include/renderer/linear.hpp
@@ -6,11 +6,13 @@
 #include "../camera.hpp"
 #include "../renderer.hpp"
 #include "../util.hpp"
-#include "../color.hpp"
 #include "../ray.hpp"
 #include "../vector3.hpp"
 
 namespace RayTracing {
+
+void writePixel(std::ofstream& outputFile, const Color& color, const int samplesPerPixel);
+Color computeRayColor(const Ray& ray, const Object& world, const int depth);
 
 class LinearRenderer: public Renderer {
 public:
@@ -38,5 +40,37 @@ public:
         outputFile.close();
     }
 };
+
+// Write the translated [0,255] value of each color component.
+void writePixel(std::ofstream& outputFile, const Color& color, const int samplesPerPixel) {
+    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+    const double r = std::sqrt(color.x / samplesPerPixel);
+    const double g = std::sqrt(color.y / samplesPerPixel);
+    const double b = std::sqrt(color.z / samplesPerPixel);
+    outputFile
+        << static_cast<int>(256 * Util::clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * Util::clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * Util::clamp(b, 0.0, 0.999)) << '\n';
+}
+
+Color computeRayColor(const Ray& ray, const Object& world, const int depth) {
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth == 0) {
+        return Color(0, 0, 0);
+    }
+    HitRecord record;
+    if (world.hit(ray, 0.001, Constants::DOUBLE_INFINITY, record)) {
+        Ray scatteredRay;
+        Color attenuation;
+        if (record.material->scatter(ray, record, attenuation, scatteredRay)) {
+            return attenuation * computeRayColor(scatteredRay, world, depth - 1);
+        } else {
+            return Color(0, 0, 0);
+        }
+    }
+    Vector3 unit = unitDirection(ray.direction);
+    double t = 0.5 * (unit.y + 1.0);
+    return ((1.0 - t) * Color(1.0, 1.0, 1.0)) + (t * Color(0.5, 0.75, 1.0));
+}
 
 }
