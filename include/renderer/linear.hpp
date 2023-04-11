1526646041fa83b5805f0930cfa9a35030f4c770
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-10 22:48:45 -0400

    Rearrange lines for more readability

--- include/renderer/linear.hpp
+++ include/renderer/linear.hpp
@@ -16,8 +16,8 @@ public:
     virtual void render(const Image& image, const Scene& scene, const Camera& camera, const int samplesPerPixel, const int maxDepth) const override {
         std::ofstream outputFile(image.fileName);
         if (outputFile.is_open()) {
-            outputFile << "P3\n" << image.width << ' ' << image.height << '\n' << "255\n";
             std::cout << "Image Dimensions: " << image.width << " x " << image.height << " | Samples Per Pixel: " << samplesPerPixel << std::endl;
+            outputFile << "P3\n" << image.width << ' ' << image.height << '\n' << "255\n";
             for (int j = image.height - 1; j >= 0; j--) {
                 std::cout << "\rProgress: " << 100 - static_cast<int>((static_cast<double>(j) / (image.height - 1)) * 100) << "% " << std::flush;
                 for (int i = 0; i < image.width; i++) {
