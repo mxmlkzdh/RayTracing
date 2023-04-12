0c1a86a425deba2763eb0cb2854141c975195c8e
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-12 16:52:49 -0400

    Add RayTracing::DiffuseLight

--- include/material/diffuse_light.hpp
+++ include/material/diffuse_light.hpp
@@ -10,15 +10,15 @@ namespace RayTracing {
 
 class DiffuseLight: public Material {
 public:
-    std::shared_ptr<Texture> emit;
+    std::shared_ptr<Texture> emitTexture;
 public:
-    DiffuseLight(const Color& emit): emit(std::make_shared<SolidColor>(emit)) {}
-    DiffuseLight(const std::shared_ptr<Texture> emit): emit(emit) {}
-    virtual bool scatter(const Ray& ray, const HitRecord& record, Color& attenuation, Ray& scatteredRay) const override {
+    DiffuseLight(const Color& emitTexture): emitTexture(std::make_shared<SolidColor>(emitTexture)) {}
+    DiffuseLight(const std::shared_ptr<Texture> emitTexture): emitTexture(emitTexture) {}
+    virtual bool scatter(const Ray&, const HitRecord&, Color&, Ray&) const override {
         return false;
     }
     virtual Color emit(const double u, const double v, const Point& point) const override {
-        return emit->value(u, v, point);
+        return emitTexture->value(u, v, point);
     }
 };
 
