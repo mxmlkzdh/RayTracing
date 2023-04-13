09e66a0fe7cd1615ab5ceaff2a188a908494eac9
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-13 19:32:12 -0400

    Fix typos

--- include/aabb.hpp
+++ include/aabb.hpp
@@ -13,8 +13,8 @@ public:
     AABB(const Point& minimum, const Point& maximum): minimum(minimum), maximum(maximum) {}
     bool hit(const Ray& ray, double initTime, double finalTime) const {
         for (std::size_t i = 0; i < 3; i++) {
-            double t0 = fmin((minimum[i] - ray.origin[i]) / ray.direction[i], (maximum[i] - ray.origin[i]) / ray.direction[i]);
-            double t1 = fmax((minimum[i] - ray.origin[i]) / ray.direction[i], (maximum[i] - ray.origin[i]) / ray.direction[i]);
+            const double t0 = fmin((minimum[i] - ray.origin[i]) / ray.direction[i], (maximum[i] - ray.origin[i]) / ray.direction[i]);
+            const double t1 = fmax((minimum[i] - ray.origin[i]) / ray.direction[i], (maximum[i] - ray.origin[i]) / ray.direction[i]);
             initTime = std::fmax(t0, initTime);
             finalTime = std::fmin(t1, finalTime);
             if (finalTime <= initTime)
