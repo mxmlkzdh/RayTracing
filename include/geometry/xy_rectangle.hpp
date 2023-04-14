5a3a3a5e26bf8f7dd29d4613d042d3e36f03e0ed
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-13 21:03:02 -0400

    Add RayTracing::XYRectangle

---
+++ include/geometry/xy_rectangle.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <memory>
+#include "../aabb.hpp"
+#include "../object.hpp"
+#include "../vector3.hpp"
+
+namespace RayTracing {
+
+class XYRectangle: public Object {
+public:
+    double x0;
+    double x1;
+    double y0;
+    double y1;
+    double k;
+    std::shared_ptr<Material> material;
+public:
+    XYRectangle(const double x0, const double x1, const double y0, const double y1, const double k, const std::shared_ptr<Material> material)
+        : x0(x0), x1(x1), y0(y0), y1(y1), k(k), material(material) {
+    }
+    virtual bool hit(const Ray& ray, const double min, const double max, HitRecord& record) const override {
+        const double t = (k - ray.origin.z) / ray.direction.z;
+        if (t < min || t > max) {
+            return false;
+        }
+        const double x = ray.origin.x + (t * ray.direction.x);
+        const double y = ray.origin.y + (t * ray.direction.y);
+        if (x < x0 || x > x1 || y < y0 || y > y1) {
+            return false;
+        }
+        record.time = t;
+        record.point = ray.at(t);
+        record.u = (x - x0) / (x1 - x0);
+        record.v = (y - y0) / (y1 - y0);
+        record.material = material;
+        record.setNormal(ray, Vector3(0, 0, 1));
+        return true;
+    };
+    virtual bool boundingBox(const double, const double, AABB& outputBox) const override {
+        // The bounding box must have non-zero width in each dimension, so pad the Z dimension a small amount.
+        outputBox = AABB(Point(x0, y0, k - 0.0001), Point(x1, y1, k + 0.0001));
+        return true;
+    }
+
+};
+
+}
