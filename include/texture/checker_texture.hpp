2be59ef16e7b5d4b36c1031290f02244de162dff
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-16 09:30:37 -0400

    Fix issue #5

--- include/texture/checker_texture.hpp
+++ include/texture/checker_texture.hpp
@@ -10,12 +10,12 @@ class CheckerTexture: public Texture {
 public:
     std::shared_ptr<Texture> even;
     std::shared_ptr<Texture> odd;
+    double size;
 public:
-    CheckerTexture() {}
-    CheckerTexture(const std::shared_ptr<Texture> even, const std::shared_ptr<Texture> odd): even(even), odd(odd) {}
-    CheckerTexture(const Color& even, const Color& odd): even(std::make_shared<SolidColor>(even)), odd(std::make_shared<SolidColor>(odd)) {}
+    CheckerTexture(const std::shared_ptr<Texture> even, const std::shared_ptr<Texture> odd, const double size = 10.0): even(even), odd(odd), size(size) {}
+    CheckerTexture(const Color& even, const Color& odd, const double size = 10): even(std::make_shared<SolidColor>(even)), odd(std::make_shared<SolidColor>(odd)), size(size) {}
     virtual Color value(const double u, const double v, const Point& point) const override {
-        const double sines = std::sin(10.0 * point.x) * std::sin(10.0 * point.y) * std::sin(10.0 * point.z);
+        const double sines = std::sin(size * point.x) * std::sin(size * point.y) * std::sin(size * point.z);
         if (sines < 0) {
             return odd->value(u, v, point);
         } else {
