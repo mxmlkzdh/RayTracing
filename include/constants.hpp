ca193dfebbcae2f4ae1177374ed91e186ad8cc25
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-18 08:01:20 -0400

    Update the value of PI

--- include/constants.hpp
+++ include/constants.hpp
@@ -6,6 +6,6 @@ namespace RayTracing::Constants {
 
 // Constants
 const double DOUBLE_INFINITY = std::numeric_limits<double>::infinity();
-const double PI = 3.1415926535897932385;
+const double PI = 3.14159265358979323846;
 
 }
