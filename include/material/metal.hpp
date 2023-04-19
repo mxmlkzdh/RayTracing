bbdd9c82f997394dc7f8b386d600ba18277c76f5
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-18 21:39:37 -0400

    Remove unnecessary unit vector

--- include/material/metal.hpp
+++ include/material/metal.hpp
@@ -13,7 +13,7 @@ public:
     Metal(const Color& color): albedo(color), perturbation(0.0) {}
     Metal(const Color& color, const double perturbation): albedo(color), perturbation(perturbation < 1 ? perturbation : 1) {}
     virtual bool scatter(const Ray& incidentRay, const HitRecord& record, Color& attenuation, Ray& scatteredRay) const override {
-        Vector3 reflected = reflect(unitDirection(incidentRay.direction), record.normal);
+        Vector3 reflected = reflect(incidentRay.direction, record.normal);
         scatteredRay = Ray(record.point, reflected + perturbation * randomInUnitSphere(), incidentRay.time);
         attenuation = albedo;
         return dot(scatteredRay.direction, record.normal) >= 0.0;
