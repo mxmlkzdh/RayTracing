843b4b86e0cad29fd8a87b743271f0997f4674e3
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-21 17:10:27 -0400

    Add Constants::AABB_PADDING

--- include/constants.hpp
+++ include/constants.hpp
@@ -7,5 +7,6 @@ namespace RayTracing::Constants {
 const double DOUBLE_INFINITY = std::numeric_limits<double>::infinity();
 const double PI = 3.14159265358979323846;
 const double NEAR_ZERO = 0.001;
+const double AABB_PADDING = 0.0001;
 
 }
