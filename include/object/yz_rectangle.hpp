77562355919d7b54c5826caef6320a5a13e50d6c
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-04-21 17:12:50 -0400

    Update YZRectangle

--- include/object/yz_rectangle.hpp
+++ include/object/yz_rectangle.hpp
@@ -3,6 +3,7 @@
 #include "../aabb.hpp"
 #include "../object.hpp"
 #include "../vector3.hpp"
+#include "../constants.hpp"
 
 namespace RayTracing {
 
@@ -12,14 +13,14 @@ public:
     double y1;
     double z0;
     double z1;
-    double k;
+    double x;
     std::shared_ptr<Material> material;
 public:
-    YZRectangle(const double y0, const double y1, const double z0, const double z1, const double k, const std::shared_ptr<Material> material)
-        : y0(y0), y1(y1), z0(z0), z1(z1), k(k), material(material) {
+    YZRectangle(const double y0, const double y1, const double z0, const double z1, const double x, const std::shared_ptr<Material> material)
+        : y0(y0), y1(y1), z0(z0), z1(z1), x(x), material(material) {
     }
     virtual bool hit(const Ray& ray, const double min, const double max, HitRecord& record) const override {
-        const double t = (k - ray.origin.x) / ray.direction.x;
+        const double t = (x - ray.origin.x) / ray.direction.x;
         if (t < min || t > max) {
             return false;
         }
@@ -33,12 +34,12 @@ public:
         record.u = (y - y0) / (y1 - y0);
         record.v = (z - z0) / (z1 - z0);
         record.material = material;
-        record.setNormal(ray, Vector3(1, 0, 0));
+        record.setNormal(ray, UnitVector(1, 0, 0));
         return true;
     };
     virtual bool boundingBox(const double, const double, AABB& outputBox) const override {
         // The bounding box must have non-zero width in each dimension, so pad the X dimension a small amount.
-        outputBox = AABB(Point(k - 0.0001, y0, z0), Point(k + 0.0001, y1, z1));
+        outputBox = AABB(Point(x - Constants::AABB_PADDING, y0, z0), Point(x + Constants::AABB_PADDING, y1, z1));
         return true;
     }
 };
